8efab4bcd1a01084c07dae5f1538fcac027fb935
Kevin-Kip <kevkiprotich@gmail.com>
2022-09-11 09:43:37 +0300

    Finished splash screen

--- app/src/main/java/com/amason/sunny/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/amason/sunny/ui/screens/SplashScreen.kt
@@ -1,22 +1,24 @@
 package com.amason.sunny.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.amason.sunny.ui.theme.BottomShape
-import com.amason.sunny.ui.theme.Primary
-import com.amason.sunny.ui.theme.ReemKufi
+import com.amason.sunny.ui.theme.*
+import com.amason.sunny.R
 
 @Composable
 fun SplashScreenUI() {
@@ -30,6 +32,18 @@ fun SplashScreenUI() {
                 ), 0, 4
             )
         }
+
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 40.dp)
+            .padding(top = 160.dp),
+        contentAlignment = Alignment.TopCenter,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_weather1),
+            contentDescription = "Weather logo"
+        )
+    }
 
     Box(contentAlignment = Alignment.BottomCenter) {
         Card(
@@ -51,9 +65,29 @@ fun SplashScreenUI() {
                     textAlign = TextAlign.Center,
                     fontWeight = FontWeight.SemiBold
                 )
-                Text(text = "Hello")
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "CLick me")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "Get weather predictions and make your life easier",
+                    fontFamily = ReemKufi,
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Center,
+                    color = LightText
+                )
+
+                Spacer(modifier = Modifier.height(36.dp))
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = White,
+                        backgroundColor = Primary,
+                    ),
+                    contentPadding = PaddingValues(horizontal = 30.dp, vertical = 10.dp),
+                    modifier = Modifier.clip(shape = Shapes.medium),
+                ) {
+                    Text(text = "Get Started", fontSize = 18.sp)
                 }
             }
         }
